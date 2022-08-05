a8a02c11b8f4a4e9e80c37293f1906734a061fe8
Aslekiel <101809691+Aslekiel@users.noreply.github.com>
2022-08-05 18:01:45 +0300

    add position on the field

---
+++ trash/trash.js
@@ -0,0 +1,57 @@
+// const battleWagon = document.querySelector(".battle-wagon");
+// const battleDestroyer = document.querySelector(".battle-destroyer");
+
+// battleWagon.addEventListener("dragstart", dragstart);
+// battleWagon.addEventListener("dragend", dragend);
+
+// const placeholders = document.querySelectorAll(".board-cell");
+
+// // battleDestroyer.addEventListener("dragstart", dragstart);
+// // battleDestroyer.addEventListener("dragend", dragend);
+
+// placeholders.forEach((placeholder) => {
+//   placeholder.addEventListener("dragover", dragover);
+//   placeholder.addEventListener("dragenter", dragenter);
+//   placeholder.addEventListener("dragleave", dragleave);
+//   placeholder.addEventListener("drop", dragdrop);
+// });
+
+// function dragstart(event) {
+//   setTimeout(() => event.target.classList.add("hide"), 0);
+// }
+
+// function dragend(event) {
+//   event.target.className = "battle-wagon";
+//   // event.target.className = "battle-destroyer";
+// }
+
+// function dragover(event) {
+//   event.preventDefault();
+//   // console.log("dragover");
+// }
+
+// function dragenter(event) {
+//   event.target.classList.add("hovered");
+//   // console.log("dragenter");
+// }
+
+// function dragleave(event) {
+//   event.target.classList.remove("hovered");
+//   // console.log("dragleave");
+// }
+
+// function dragdrop(event) {
+//   event.target.append(battleWagon);
+//   // event.target.append(battleDestroyer);
+// }
+
+//////////////////////////////
+//CSS
+
+// .hide {
+//     display: none;
+//   }
+
+//   .hovered {
+//     border: 1px solid red;
+//   }
